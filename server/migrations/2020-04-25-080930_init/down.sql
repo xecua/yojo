2b1ad673f899cf9845d9e13c38a1bdf79640d49f
xecua <courser4848@gmail.com>
2020-04-25 18:42:09 +0900

    migration

---
+++ server/migrations/2020-04-25-080930_init/down.sql
@@ -0,0 +1,4 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE `tweets`;
+DROP TABLE `tags`;
+DROP TABLE `tweets_to_tags`;
---
+++ server/src/schema.rs
@@ -0,0 +1,31 @@
+table! {
+    tags (id) {
+        id -> Varchar,
+        tag -> Varchar,
+    }
+}
+
+table! {
+    tweets (id) {
+        id -> Varchar,
+        tweet_id -> Nullable<Varchar>,
+        comment -> Varchar,
+    }
+}
+
+table! {
+    tweets_to_tags (id) {
+        id -> Varchar,
+        tweets_id -> Varchar,
+        tags_id -> Varchar,
+    }
+}
+
+joinable!(tweets_to_tags -> tags (tags_id));
+joinable!(tweets_to_tags -> tweets (tweets_id));
+
+allow_tables_to_appear_in_same_query!(
+    tags,
+    tweets,
+    tweets_to_tags,
+);
