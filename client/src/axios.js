89f97a5a88d14aec409f0c2dc095eff5c9fc918d
xecua <courser4848@gmail.com>
2020-05-03 00:11:39 +0900

    "->'

---
+++ client/src/axios.js
@@ -0,0 +1,4 @@
+import __axios__ from 'axios';
+
+export const axios = __axios__;
+axios.defaults.baseURL = 'http://localhost:3000';
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from './axios';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
+Vue.use(axios);
+Vue.use(Buefy);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
