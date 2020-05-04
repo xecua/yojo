b196ac3361388f6f83a7e7dbbf95df86292ac54c
xecua <courser4848@gmail.com>
2020-05-04 17:33:22 +0900

    検索

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <div id="app">
     <section class="section">
-      <div class="container">
-        <register-form @tweet-posted="updateTweet" />
+      <div class="columns is-centered">
+        <b-field class="column is-6">
+          <b-input
+            v-model="searchQuery"
+            rounded
+            icon="magnify" />
+        </b-field>
       </div>
     </section>
-    <section class="section">
-      <div class="container">
-        <template v-for="(tweet, i) in tweets">
-          <tweet-card
-            :key="i"
-            v-bind="tweet" />
-        </template>
-      </div>
-    </section>
+
+    <template v-if="searchQuery.length > 0">
+      <section class="section">
+        <div class="container">
+          <template v-for="(tweet, i) in filteredTweets">
+            <tweet-card
+              :key="i"
+              v-bind="tweet" />
+          </template>
+        </div>
+      </section>
+    </template>
+
+    <template v-else>
+      <section class="section">
+        <div class="container">
+          <register-form @tweet-posted="updateTweet" />
+        </div>
+      </section>
+      <section class="section">
+        <div class="container">
+          <template v-for="(tweet, i) in tweets">
+            <tweet-card
+              :key="i"
+              v-bind="tweet" />
+          </template>
+        </div>
+      </section>
+    </template>
   </div>
 </template>
 
@@ -28,18 +53,32 @@ export default {
   },
   data() {
     return {
+      searchQuery: '',
       tweets: [] // TweetDetail[]
     };
   },
+  computed: {
+    filteredTweets() {
+      const parser = new DOMParser();
+      return this.tweets.filter(tweet => 
+        parser.parseFromString(tweet.html, 'text/html')
+          .getElementsByTagName('p')[0]
+          .innerText
+          .includes(this.searchQuery)
+      );
+    }
+  },
   created() {
     this.updateTweet();
+  },
+  updated() {
+    // eslint-disable-next-line no-undef
+    twttr.widgets.load(); // from widgets.js
   },
   methods: {
     updateTweet() {
       this.$axios.get('/tweets').then(resp => {
         this.tweets = resp.data;
-        // eslint-disable-next-line no-undef
-        twttr.widgets.load(); // from widgets.js
       });
     }
   }
